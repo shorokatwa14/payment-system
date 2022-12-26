e63954724ba7f573ad7574c3c2397540e2bdf62d
Shorok Atwa <72991188+shorokatwa14@users.noreply.github.com>
2022-12-26 22:34:45 +0200

    src

---
+++ src/payment/service/PaymentService.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package payment.service;
+
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.*;
+
+/**
+ *
+ * @author COMPUMARTS
+ */
+public class PaymentService {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    }
+}
